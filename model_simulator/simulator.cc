b29c5c2f428b2032dacdba9de56434f37f608d93
gkiene <gerd.kiene@kip.uni-heidelberg.de>
2016-12-09 16:13:21 +0100

    added class structure to program

--- model_simulator/simulator.cc
+++ model_simulator/simulator.cc
@@ -7,18 +7,16 @@
 int const rows = 10;
 int const columns = 10;
 
-template <typename T>
 struct neuron_constants
 {
-    T i_syn_0;
-    T e_l;
-    T tau_l;
-    T tau_syn;
-    T threshold;
-    T reset_potential;
-    T refrac_period;
+    double i_syn_0;
+    double e_l;
+    double tau_l;
+    double tau_syn;
+    double threshold;
+    double reset_potential;
+    double refrac_period;
 };
-
 
 struct neuron_state_var
 {
@@ -27,9 +25,8 @@ struct neuron_state_var
     int fired;
     int refrac;
     int refrac_count;
-    neuron_constants<double> constants;
+    neuron_constants constants;
 };
-
 
 struct neural_net
 {
@@ -37,7 +34,37 @@ struct neural_net
     neuron_state_var neurons[columns];
 };
 
-void read_in_config(neural_net *nn)
+
+class Neural_net_c
+{
+    neural_net nn;
+    double t;
+    double d_t;
+    int num_sim_steps;
+    int step;
+    int spiketimes[200000];
+
+
+    public:
+    void read_in_config();
+    double external_current(int);
+    double synaptic_input_right_side(double, double, int, int*, int);
+    double lif_right_side(double, int, int);
+    neuron_state_var explicit_euler_step(double, int, int*, int, double);
+    neuron_state_var runge_kutta_step(double, int, int*, int, double);
+    double sum_weigths_in_timestep(int);
+    int rand_val_0_1(double p);
+    void init_rdm_spike_train(int, int*, double);
+    void evolve_net();
+    void neuron_config_hom_setup();
+    void weifgth_config_non_self_all_all();
+    void set_initial_conditions();
+    void print_config();
+    void init_simulator();
+};
+
+
+void Neural_net_c::read_in_config()
 {
     FILE *fr;
     char weigth_in[] = "weigth_config.data";
@@ -46,7 +73,7 @@ void read_in_config(neural_net *nn)
     {
         for (int j_column=0; j_column<columns; j_column++)
         {
-            fscanf(fr, "%lf ", &nn->weigths[i_row][j_column]);
+            fscanf(fr, "%lf ", &this->nn.weigths[i_row][j_column]);
         }
     }
     fclose(fr);
@@ -56,19 +83,19 @@ void read_in_config(neural_net *nn)
 
     for (int j_column = 0; j_column<columns; j_column++)
     {
-        fscanf(fr, "%lf ", &nn->neurons[j_column].constants.e_l);
-        fscanf(fr, "%lf ", &nn->neurons[j_column].constants.i_syn_0);
-        fscanf(fr, "%lf ", &nn->neurons[j_column].constants.tau_l);
-        fscanf(fr, "%lf ", &nn->neurons[j_column].constants.tau_syn);
-        fscanf(fr, "%lf ", &nn->neurons[j_column].constants.reset_potential);
-        fscanf(fr, "%lf ", &nn->neurons[j_column].constants.threshold);
-        fscanf(fr, "%lf ", &nn->neurons[j_column].constants.refrac_period);
+        fscanf(fr, "%lf ", &this->nn.neurons[j_column].constants.e_l);
+        fscanf(fr, "%lf ", &this->nn.neurons[j_column].constants.i_syn_0);
+        fscanf(fr, "%lf ", &this->nn.neurons[j_column].constants.tau_l);
+        fscanf(fr, "%lf ", &this->nn.neurons[j_column].constants.tau_syn);
+        fscanf(fr, "%lf ", &this->nn.neurons[j_column].constants.reset_potential);
+        fscanf(fr, "%lf ", &this->nn.neurons[j_column].constants.threshold);
+        fscanf(fr, "%lf ", &this->nn.neurons[j_column].constants.refrac_period);
     }
     fclose(fr);
 }
 
 
-double external_current(int step)
+double Neural_net_c::external_current(int step)
 {
     double return_value;
     if(step < 500)
@@ -84,100 +111,97 @@ double external_current(int step)
     return return_value;
 }
 
-double synaptic_input_right_side(double syn_current_input, neuron_constants<double> constants, int step, int *spiketimes, double weigthsum)
+double Neural_net_c::synaptic_input_right_side(double current, double weigthsum, int n_i, int *spiketimes, int step)
 {
-    return -1.0/constants.tau_syn * syn_current_input + constants.i_syn_0 * spiketimes[step] + constants.i_syn_0 * weigthsum;
+    return -1.0/this->nn.neurons[n_i].constants.tau_syn * current+ this->nn.neurons[n_i].constants.i_syn_0 * spiketimes[step] + this->nn.neurons[n_i].constants.i_syn_0 * weigthsum;
 }
 
-
-double lif_right_side(double v, neuron_constants<double> constants, int step, double (*current_input)(int), double syn_current_input)
+double Neural_net_c::lif_right_side(double voltage, int n_i, int step)
 {
-    return 1.0/constants.tau_l * (constants.e_l - v) + current_input(step) + syn_current_input;
+    return 1.0/this->nn.neurons[n_i].constants.tau_l * (this->nn.neurons[n_i].constants.e_l - voltage) + external_current(step) + this->nn.neurons[n_i].syn_current;
 }
 
-neuron_state_var explicit_euler_step(neuron_state_var neuron_state, neuron_constants<double> constants, const double d_t, int step, double (*current_input)(int), int *spiketimes, double weigthsum)
+neuron_state_var Neural_net_c::explicit_euler_step(double weigthsum, int n_i, int *spiketimes, int step, double d_t)
 {
-    double k_1_s = synaptic_input_right_side(neuron_state.syn_current, constants, step, spiketimes, weigthsum);
-    neuron_state.syn_current = neuron_state.syn_current + k_1_s * d_t;
+    double k_1_s = synaptic_input_right_side(this->nn.neurons[n_i].syn_current, weigthsum, n_i, spiketimes, step);
+    this->nn.neurons[n_i].syn_current = this->nn.neurons[n_i].syn_current + k_1_s * d_t;
 
-    double k_1_n = lif_right_side(neuron_state.voltage, constants, step, current_input, neuron_state.syn_current);
-    neuron_state.voltage = neuron_state.voltage + k_1_n * d_t;
+    double k_1_n = lif_right_side(this->nn.neurons[n_i].voltage, n_i, step);
+    this->nn.neurons[n_i].voltage = this->nn.neurons[n_i].voltage + k_1_n * d_t;
 
-    if (neuron_state.refrac)
+    if (this->nn.neurons[n_i].refrac)
     {
-        neuron_state.voltage = constants.reset_potential;
-        neuron_state.refrac_count += 1;
-        if (neuron_state.refrac_count > constants.refrac_period)
-            neuron_state.refrac = 0;
+        this->nn.neurons[n_i].voltage = this->nn.neurons[n_i].constants.reset_potential;
+        this->nn.neurons[n_i].refrac_count += 1;
+        if (this->nn.neurons[n_i].refrac_count > this->nn.neurons[n_i].constants.refrac_period)
+            this->nn.neurons[n_i].refrac = 0;
     }
 
-    if (neuron_state.voltage > constants.threshold)
+    if (this->nn.neurons[n_i].voltage > this->nn.neurons[n_i].constants.threshold)
     {
-        neuron_state.voltage = constants.reset_potential;
-        neuron_state.fired = 1;
-        neuron_state.refrac = 1;
-        neuron_state.refrac_count = 0;
-    }
-    else
-        neuron_state.fired = 0;
-    return neuron_state;
-}
-
-
-neuron_state_var runge_kutta_step(neuron_state_var neuron_state, neuron_constants<double> constants, const double d_t, int step, double (*current_input)(int), int *spiketimes, double weigthsum)
-{
-    double k_1_s = synaptic_input_right_side(neuron_state.syn_current, constants, step, spiketimes, weigthsum);
-    double k_2_s = synaptic_input_right_side(neuron_state.syn_current + k_1_s * d_t, constants, step, spiketimes, weigthsum);
-
-    neuron_state.syn_current = neuron_state.syn_current + (k_1_s + k_2_s) / 2.0 * d_t;
-
-    double k_1_n = lif_right_side(neuron_state.voltage, constants, step, current_input, neuron_state.syn_current);
-    double k_2_n = lif_right_side(neuron_state.voltage + k_1_n * d_t, constants, step + 1, current_input, neuron_state.syn_current);
-
-    neuron_state.voltage = neuron_state.voltage + (k_1_n + k_2_n) / 2.0 * d_t;
-
-    if (neuron_state.refrac)
-    {
-        neuron_state.voltage = constants.reset_potential;
-        neuron_state.refrac_count += 1;
-        if (neuron_state.refrac_count > constants.refrac_period / d_t)
-            neuron_state.refrac = 0;
-    }
-
-    if (neuron_state.voltage > constants.threshold)
-    {
-        neuron_state.voltage = constants.reset_potential;
-        neuron_state.fired = 1;
-        neuron_state.refrac = 1;
-        neuron_state.refrac_count = 0;
+        this->nn.neurons[n_i].voltage = this->nn.neurons[n_i].constants.reset_potential;
+        this->nn.neurons[n_i].fired = 1;
+        this->nn.neurons[n_i].refrac = 1;
+        this->nn.neurons[n_i].refrac_count = 0;
     }
     else
-        neuron_state.fired = 0;
-
-
-    return neuron_state;
+        this->nn.neurons[n_i].fired = 0;
+    return this->nn.neurons[n_i];
 }
 
-double sum_weigths_in_timestep(neural_net nn, int neuron)
+
+neuron_state_var Neural_net_c::runge_kutta_step(double weigthsum, int n_i, int *spiketimes, int step, double d_t)
+{
+    double k_1_s = synaptic_input_right_side(this->nn.neurons[n_i].syn_current, weigthsum, n_i, spiketimes, step);
+    double k_2_s = synaptic_input_right_side(this->nn.neurons[n_i].syn_current + k_1_s * d_t, weigthsum, n_i, spiketimes, step);
+
+    this->nn.neurons[n_i].syn_current = this->nn.neurons[n_i].syn_current + (k_1_s + k_2_s) / 2.0 * d_t;
+
+    double k_1_n = lif_right_side(this->nn.neurons[n_i].voltage, n_i, step);
+    double k_2_n = lif_right_side(this->nn.neurons[n_i].voltage + k_1_n * d_t, n_i, step);
+
+    this->nn.neurons[n_i].voltage = this->nn.neurons[n_i].voltage + (k_1_n + k_2_n) / 2.0 * d_t;
+
+    if (this->nn.neurons[n_i].refrac)
+    {
+        this->nn.neurons[n_i].voltage = this->nn.neurons[n_i].constants.reset_potential;
+        this->nn.neurons[n_i].refrac_count += 1;
+        if (this->nn.neurons[n_i].refrac_count > this->nn.neurons[n_i].constants.refrac_period / d_t)
+            this->nn.neurons[n_i].refrac = 0;
+    }
+
+    if (this->nn.neurons[n_i].voltage > this->nn.neurons[n_i].constants.threshold)
+    {
+        this->nn.neurons[n_i].voltage = this->nn.neurons[n_i].constants.reset_potential;
+        this->nn.neurons[n_i].fired = 1;
+        this->nn.neurons[n_i].refrac = 1;
+        this->nn.neurons[n_i].refrac_count = 0;
+    }
+    else
+        this->nn.neurons[n_i].fired = 0;
+    return this->nn.neurons[n_i];
+}
+
+double Neural_net_c::sum_weigths_in_timestep(int neuron)
 {
     double weight_sum = 0;
     for(int i=0; i<rows; i++)
     {
-        if(nn.neurons[neuron].fired)
+        if(this->nn.neurons[neuron].fired)
         {
-            weight_sum += nn.weigths[neuron][i];
+            weight_sum += this->nn.weigths[neuron][i];
         }
     }
     return weight_sum;
 }
 
-int rand_val_0_1(double p)
+int Neural_net_c::rand_val_0_1(double p)
 {
     double rand_double = (double)rand() / RAND_MAX;
     return rand_double > (1 - p);
 }
 
-void init_rdm_spike_train(int num_sim_steps, int *spiketimes, double p)
+void Neural_net_c::init_rdm_spike_train(int num_sim_steps, int *spiketimes, double p)
 {
     for(int i=0; i<num_sim_steps; i++)
     {
@@ -185,69 +209,72 @@ void init_rdm_spike_train(int num_sim_steps, int *spiketimes, double p)
     }
 }
 
-void evolve_net(neural_net nn, double t, double d_t, int num_sim_steps, int step, double (*current_input)(int), int *spiketimes)
+void Neural_net_c::evolve_net()
 {
     FILE *fp;
     char output[] = "simulator_output.sim_data";
     fp = fopen(output,"w");
+    printf("%d \n", this->num_sim_steps);
 
     double weigthsum;
-    for (int i=0; i<num_sim_steps; i++)
+    for (int i=0; i<this->num_sim_steps; i++)
     {
         for (int j=0; j < columns; j++){
-            weigthsum = sum_weigths_in_timestep(nn, j);
-            nn.neurons[j] = runge_kutta_step(nn.neurons[j], nn.neurons[j].constants, d_t, step, current_input, spiketimes, weigthsum);
-            fprintf(fp, "%f ", nn.neurons[j].voltage);
-            fprintf(fp, "%f ", nn.neurons[j].syn_current);
-            fprintf(fp, "%d ", nn.neurons[j].fired);
+            weigthsum = sum_weigths_in_timestep(j);
+            this->nn.neurons[j] = runge_kutta_step(weigthsum, j, this->spiketimes, this->step, this->d_t);
+            fprintf(fp, "%f ", this->nn.neurons[j].voltage);
+            fprintf(fp, "%f ", this->nn.neurons[j].syn_current);
+            fprintf(fp, "%d ", this->nn.neurons[j].fired);
         }
-        t += d_t;
-        step += 1;
-        fprintf(fp, "%f\n", t);
+        this->t += this->d_t;
+        this->step += 1;
+        fprintf(fp, "%f\n", this->t);
     }
     fclose(fp);
 }
 
-void neuron_config_hom_setup(neural_net *nn)
+/*
+void Neural_net_c::neuron_config_hom_setup()
 {
     for (int i=0; i<columns; i++)
     {
-       nn->neurons[i].constants.e_l = 0.0;
-       nn->neurons[i].constants.i_syn_0 = 4000.0;
-       nn->neurons[i].constants.tau_l = 0.01;
-       nn->neurons[i].constants.tau_syn = 3.0;
-       nn->neurons[i].constants.reset_potential= 0.0;
-       nn->neurons[i].constants.threshold = 1.2;
-       nn->neurons[i].constants.refrac_period = 1;
+       this->nn.neurons[i].constants.e_l = 0.0;
+       this->nn.neurons[i].constants.i_syn_0 = 4000.0;
+       this->nn.neurons[i].constants.tau_l = 0.01;
+       this->nn.neurons[i].constants.tau_syn = 3.0;
+       this->nn.neurons[i].constants.reset_potential= 0.0;
+       this->nn.neurons[i].constants.threshold = 1.2;
+       this->nn.neurons[i].constants.refrac_period = 1;
     }
 }
 
-void weigth_config_non_self_all_all(neural_net *nn)
+void Neural_net_c::weigth_config_non_self_all_all()
 {
     for(int i=0; i<columns; i++)
     {
         for(int j=0; j<rows; j++)
         {
             if (i != j)
-                nn->weigths[i][j] = 0.0001;
+                this->nn.weigths[i][j] = 0.0001;
             if (i == j)
-                nn->weigths[i][j] = 0;
+                this->nn.weigths[i][j] = 0;
         }
     }
 }
+*/
 
-void set_initial_conditions(neural_net *nn)
+void Neural_net_c::set_initial_conditions()
 {
     for (int i=0; i<columns; i++)
     {
-        nn->neurons[i].voltage = 0.0;
-        nn->neurons[i].syn_current = 0.0;
-        nn->neurons[i].refrac = 0;
-        nn->neurons[i].refrac_count = 0;
+        this->nn.neurons[i].voltage = 0.0;
+        this->nn.neurons[i].syn_current = 0.0;
+        this->nn.neurons[i].refrac = 0;
+        this->nn.neurons[i].refrac_count = 0;
     }
 }
 
-void print_config(neural_net *nn)
+void Neural_net_c::print_config()
 {
     printf("\nPrinting the weigth matrix \n");
     printf("-----------------------------\n");
@@ -255,42 +282,39 @@ void print_config(neural_net *nn)
     {
         for (int j=0; j<columns; j++)
         {
-            printf("%f ", nn->weigths[i][j]);
+            printf("%f ", this->nn.weigths[i][j]);
             if (j == columns - 1)
                 printf("\n");
         }
     }
     printf("\nPrint config for neuron 0 \n");
     printf("-----------------------------\n");
-    printf("e_l:           %f \n", nn->neurons[0].constants.e_l = 0.0);
-    printf("i_syn:         %f \n", nn->neurons[0].constants.i_syn_0 = 4000.0);
-    printf("tau_l          %f \n", nn->neurons[0].constants.tau_l = 0.01);
-    printf("tau_syn        %f \n", nn->neurons[0].constants.tau_syn = 3.0);
-    printf("reset_potential%f \n", nn->neurons[0].constants.reset_potential= 0.0);
-    printf("threshold:     %f \n", nn->neurons[0].constants.threshold = 1.2);
-    printf("refrac_period: %f \n", nn->neurons[0].constants.refrac_period = 1);
+    printf("e_l:           %f \n", this->nn.neurons[0].constants.e_l = 0.0);
+    printf("i_syn:         %f \n", this->nn.neurons[0].constants.i_syn_0 = 4000.0);
+    printf("tau_l          %f \n", this->nn.neurons[0].constants.tau_l = 0.01);
+    printf("tau_syn        %f \n", this->nn.neurons[0].constants.tau_syn = 3.0);
+    printf("reset_potential%f \n", this->nn.neurons[0].constants.reset_potential= 0.0);
+    printf("threshold:     %f \n", this->nn.neurons[0].constants.threshold = 1.2);
+    printf("refrac_period: %f \n", this->nn.neurons[0].constants.refrac_period = 1);
+}
+
+void Neural_net_c::init_simulator()
+{
+    this->num_sim_steps = 200000;
+    this->step = 0;
+    this->d_t = 0.1e-3;
+    this->t = 0.0;
+    init_rdm_spike_train(this->num_sim_steps, this->spiketimes, 1500.0 * this->d_t);
 }
 
 int main()
 {
-    int num_sim_steps = 200000;
-    int step = 0;
+    Neural_net_c nn;
+    nn.init_simulator();
+    nn.set_initial_conditions();
+    nn.read_in_config();
+    
+    nn.print_config();
 
-    // init the neural net
-    neural_net nn;
-
-    // initializing the constants
-    //neuron_config_hom_setup(&nn);
-    //weigth_config_non_self_all_all(&nn);
-    read_in_config(&nn);
-    set_initial_conditions(&nn);
-    //print_config(&nn);
-    double d_t = 0.1e-3;  // count time in s
-
-    // initial conditions
-    double t = 0.0;
-    int spiketimes[num_sim_steps];
-    init_rdm_spike_train(num_sim_steps, spiketimes, 1500.0 * d_t); // average of 10 spikes per 1s
-
-    evolve_net(nn, t, d_t, num_sim_steps, step, external_current, spiketimes);
+    nn.evolve_net();
 }
